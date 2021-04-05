66e70880890f6e33f00d495f024c53444ee79df4
engcristian <eng.cristian.silva@gmail.com>
2021-04-04 23:18:12 -0300

    removed Django

--- Django/project_agenda/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from core.models import Evento
-
-class EventoAdmin(admin.ModelAdmin):
-    list_display = ('id','titulo', 'data_evento','data_criacao')
-    list_filter = ('titulo',)
-admin.site.register(Evento, EventoAdmin)
